93d1bb866371b7871fc8b4efcd6b28d125f9d7b5
apatheticreplicant <burningcelery@protonmail.ch>
2023-02-19 16:45:09 -0700

    updated dogjekt

--- toys/dogjekt/dogjekt.js
+++ toys/dogjekt/dogjekt.js
@@ -1,6 +1,8 @@
 const dogimg = document.getElementById("dogimg");
 const spinner = document.getElementById("spinner");
 const breedSelect = document.getElementById("pupselect");
+const browseButton = document.getElementById("randomdog");
+const nxtBtn = document.getElementById("nextdog")
 
 async function init() {
 
@@ -15,15 +17,25 @@ async function init() {
 	}
 
 	breedSelect.innerHTML = breedOptions;
-
-
-
-  	const randomDog = await fetch("https://dog.ceo/api/breeds/image/random");
-  	const randomDogJson = await randomDog.json();
-
-  	dogimg.src = randomDogJson.message;
-
 	
+	// page start random 
+	async function pageStart() {
+
+	const randomDogList = await fetch("https://dog.ceo/api/breeds/list/random")
+	const randomDogListJson = await randomDogList.json();
+	breedSelect.value = randomDogListJson.message;
+
+	let breed = breedSelect.value  
+	const dog = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`);
+	const dogJson = await dog.json();
+
+	dogimg.classList.remove("display");
+	spinner.classList.add("display");
+	dogimg.src = dogJson.message;
+
+	}
+	pageStart();
+	//
 
 	breedSelect.addEventListener("change", handleBreedChange);
 
@@ -31,9 +43,10 @@ async function init() {
 		dogimg.classList.add("display");
 		spinner.classList.remove("display")
 	}); 
-}
+
 
 async function handleBreedChange(event) {
+
 	const breed = event.target.value;
 
 	dogimg.classList.remove("display");
@@ -44,8 +57,32 @@ async function handleBreedChange(event) {
 	const dogJson = await dog.json();
 	
 	dogimg.src = dogJson.message;
+}
 	
+// "random" dog button
+	
+browseButton.addEventListener("click", pageStart);
+
+
+
+// "next" dog button, in breed
+
+	nxtBtn.addEventListener("click", handleBreedBrowse);
+
+ async function handleBreedBrowse() { 
+	
+	const breed = breedSelect.value;
+
+	dogimg.classList.remove("display");
+	spinner.classList.add("display");
+
+	const dog = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`);
+	const dogJson = await dog.json();
+	
+	dogimg.src = dogJson.message;
 }
 
 
+}
 init();
+
